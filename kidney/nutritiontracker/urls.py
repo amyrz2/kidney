65502f29bafe4a351abc633d5684f5f1b7b69934
wirickad <wirickad@gmail.com>
2022-11-28 17:59:20 -0700

    1st push

--- kidney/nutritiontracker/urls.py
+++ kidney/nutritiontracker/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import indexPageView
+from . import views 
 
 urlpatterns = [
-    path("", indexPageView, name="index"), 
+    path("", views.indexPageView, name="index"), 
+    path("about/", views.logEntryPageView, name="logEntry"),    
+    path("login/", views.loginPageView, name="login"), 
+    #path("profile/<str:person_name>", views.profilePageView, name="profile")
 
 ]    
+
+
+#Ignore the line below for now
+#path('<int:pk>/', views.DetailView.as_view(), name='detail'),
--- kidney/nutritiontracker/views.py
+++ kidney/nutritiontracker/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
+#from django.views import generic
 # Create your views here.
 
 def indexPageView(request) :
     return render(request, 'nutritiontracker/index.html')   
+
+def loginPageView(request) :
+    return render(request, 'nutritionTracker/login.html')
+
+def dailyNutritionView(request) :
+    return render(request, 'nutritionTracker/dailyBreakdown.html')
+
+def logEntryPageView(request) :
+    return render(request, 'nutritionTracker/logEntry.html')
